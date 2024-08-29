8fbc3e4d93bcf0a40bb3da5c7ed5139947ca688e
Karla Daiany Oliveira <karla.oliveira@twygo.com>
2024-08-29 11:53:34 -0300

    Arquivo movido para PO e ajustado tipo dos elementos

--- cypress/support/pageObjects/formComentariosComunidade.js
+++ cypress/support/pageObjects/formComentariosComunidade.js
@@ -1,30 +1,26 @@
 class formComentariosComunidade{
 	// Mapeamento de elementos da tela de criação de comentários da comunidade (Edição de Espaço)
-
-
 	elementos = {
 		comentario:{
             seletor: 'textarea[name="text_comment"]',
-            tipo: 'textarea'
+            tipo: 'input'
 		},
         btnAdicionarComentario: {
             seletor: 'div#comment i.pointer far fa-comment',
-            tipo: 'i'
+            tipo: 'button'
         },
 		btnVerComentarios: {
             seletor: 'a.pointer i.fas fa-sort-down',
-            tipo: 'i'
+            tipo: 'button'
         },
         btnFecharComentarios: {
             seletor: 'a.pointer i.fas fa-sort-down open',
-            tipo: 'i'
+            tipo: 'button'
         },
         btnPublicar: {
             seletor: '.button btn btn-primary waves-effect',
             tipo: 'button'
         }
-
 	}
-
 }
 export default new formComentariosComunidade
